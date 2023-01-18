f89eaecbce8cec5553cf031a62d95f64a501bba9
Carolina Fuenzalida <108352521+CarolinaFuenzalida@users.noreply.github.com>
2023-01-18 15:24:34 -0300

    Update Lobby.jsx

--- src/pages/Lobby/Lobby.jsx
+++ src/pages/Lobby/Lobby.jsx
@@ -7,7 +7,7 @@ export const Lobby = () => {
   return (
     <div  className="divLobby">
         <picture className="Logo"> 
-        <img  src="src\img\SanrioCoffeeLogo1.png" alt="Logo de Cafeteria, contiene personaje de Sanrio" />
+        <img  src="https://cdn.discordapp.com/attachments/1037900045095284787/1065335756639453234/SanrioCoffeeLogo1.png" alt="Logo de Cafeteria, contiene personaje de Sanrio" />
         </picture>
         <Link to="/Waiter" className="buttonRouter"><button className="buttonL">Mesero</button></Link>
          <Link to="/Kitchen" className="buttonRouter"><button className="buttonL">Cocina</button></Link>
