5a29f73c968fa38b2071f70b06325d56fc42359b
Carolina Fuenzalida <108352521+CarolinaFuenzalida@users.noreply.github.com>
2023-01-18 15:30:49 -0300

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -10,12 +10,12 @@ export const NavBar = () => {
 return location.pathname == "/Waiter" ? ((
     <header className="Top">
       <Link to="/" className="buttonStyle"> <button className="buttonR" > {" "}<BiArrowBack/>{" "}</button></Link>
-      <img src="src\img\SanrioCoffeeLogo1.png"></img>
+      <img src="https://cdn.discordapp.com/attachments/1037900045095284787/1065335756639453234/SanrioCoffeeLogo1.png"></img>
       <Link to="/Kitchen" className="buttonStyle"><button className="buttonR">{" "}<GiCookingPot/>{" "}</button></Link>
       </header>
     ) ): (
       <header className="TopKitchen">
-     <Link to="/"> <button className="buttonRouter" id="backMain"><img src="src\img\SanrioCoffeeLogo1.png"></img> {" "}<BiArrowBack/>{" "}
+     <Link to="/"> <button className="buttonRouter" id="backMain"><img src="https://cdn.discordapp.com/attachments/1037900045095284787/1065335756639453234/SanrioCoffeeLogo1.png"></img> {" "}<BiArrowBack/>{" "}
        </button></Link>
       </header>
     )
